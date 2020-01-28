3146a3cb7624bf89e30d2dceff9aea3b6261deb9
Kyle Huey <khuey@kylehuey.com>
2020-01-28 13:54:35 -0800

    Upgrade to latest proc macro stuff.

--- enum_index_derive/src/lib.rs
+++ enum_index_derive/src/lib.rs
@@ -2,43 +2,43 @@ extern crate syn;
 #[macro_use]
 extern crate quote;
 extern crate proc_macro;
-use proc_macro::TokenStream;
+extern crate proc_macro2;
+use quote::format_ident;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(EnumIndex)]
-pub fn enum_index(input: TokenStream) -> TokenStream {
-    let s = input.to_string();
-    let ast = syn::parse_derive_input(&s).unwrap();
+pub fn enum_index(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
 
     let tokens = impl_enum_index(&ast);
-    tokens.parse().unwrap()
+    proc_macro::TokenStream::from(tokens)
 }
 
 
-fn impl_enum_index(ast: &syn::DeriveInput) -> quote::Tokens {
+fn impl_enum_index(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     let name = &ast.ident;
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
-    let variants = match ast.body {
-        syn::Body::Enum(ref v) => v,
+    let variants = match ast.data {
+        syn::Data::Enum(ref v) => &v.variants,
         _ => panic!("EnumIndex can be only implemented for Enums")
     };
 
     let mut matches = Vec::new();
 
     for variant in variants {
-        use syn::VariantData::*;
+        use syn::Fields::*;
         let ident = &variant.ident;
 
-        let params = match variant.data {
-            Unit => quote::Ident::from(""),
-            Tuple(..) => quote::Ident::from("(..)"),
-            Struct(..) => quote::Ident::from("{..}")
+        let params = match variant.fields {
+            Unit => format_ident!(""),
+            Unnamed(..) => format_ident!("(..)"),
+            Named(..) => format_ident!("{{..}}")
         };
 
         let index = matches.len();
         matches.push(quote!{ #name::#ident #params => #index});
     }
-
 
     quote!{
         impl #impl_generics enum_index::EnumIndex for #name #ty_generics #where_clause {
@@ -53,21 +53,20 @@ fn impl_enum_index(ast: &syn::DeriveInput) -> quote::Tokens {
 
 
 #[proc_macro_derive(IndexEnum)]
-pub fn index_enum(input: TokenStream) -> TokenStream {
-    let s = input.to_string();
-    let ast = syn::parse_derive_input(&s).unwrap();
+pub fn index_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
 
     let tokens = impl_index_enum(&ast);
-    tokens.parse().unwrap()
+    proc_macro::TokenStream::from(tokens)
 }
 
 
-fn impl_index_enum(ast: &syn::DeriveInput) -> quote::Tokens {
+fn impl_index_enum(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     let name = &ast.ident;
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
-    let variants = match ast.body {
-        syn::Body::Enum(ref v) => v,
+    let variants = match ast.data {
+        syn::Data::Enum(ref v) => &v.variants,
         _ => panic!("IndexEnum can be only implemented for Enums")
     };
 
@@ -75,23 +74,23 @@ fn impl_index_enum(ast: &syn::DeriveInput) -> quote::Tokens {
     let mut index : usize = 0;
 
     for variant in variants {
-        use syn::VariantData::*;
+        use syn::Fields::*;
         let ident = &variant.ident;
         match variant.discriminant {
             None => {},
-            Some(syn::ConstExpr::Lit(syn::Lit::Int(i, _))) => {
-                index = i as usize;
+            Some((_, syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Int(ref i), .. }))) => {
+                index = i.base10_parse::<usize>().unwrap();
             },
             Some(_) => panic!("Unhandled enum discriminant!"),
         };
 
-        match variant.data {
+        match variant.fields {
             Unit => {
                 index_matches.push(quote! { #index => Some(#name::#ident) });
             },
-            Tuple(ref fields) => {
+            Unnamed(ref fields) => {
                 let mut initialized_fields = Vec::new();
-                for field in fields {
+                for field in &fields.unnamed {
                     let field_type = &field.ty;
                     initialized_fields.push(quote! { #field_type::default()} );
                 }
@@ -99,9 +98,9 @@ fn impl_index_enum(ast: &syn::DeriveInput) -> quote::Tokens {
                     #index => Some(#name::#ident(#(#initialized_fields),*))
                 });
             }
-            Struct(ref fields) => {
+            Named(ref fields) => {
                 let mut initialized_fields = Vec::new();
-                for field in fields {
+                for field in &fields.named {
                     let field_name = &field.ident;
                     let field_type = &field.ty;
                     initialized_fields.push(quote! { #field_name: #field_type::default()});
@@ -111,7 +110,6 @@ fn impl_index_enum(ast: &syn::DeriveInput) -> quote::Tokens {
         }
         index += 1;
     }
-
 
     quote!{
         impl #impl_generics enum_index::IndexEnum for #name #ty_generics #where_clause {
